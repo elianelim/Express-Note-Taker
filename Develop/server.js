924720ebd8baf30e7190cb41d4d2a8cd41a7bba8
elianelim <elianelim.sf@gmail.com>
2023-02-27 22:19:15 +0800

    displaying of index and notes .html for GET route

--- Develop/server.js
+++ Develop/server.js
@@ -11,51 +11,67 @@ const app = express();
 //Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
+
+//USE static file
+app.use(express.static('public'));
+
+//GET Route to index/home page
+app.get ('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 //Wildcard route to direct users to index/home page
-app.get ('*', (req, res) =>
-    res.sendFile(__dirname, 'public/assets/index.html'));
+app.get ('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
 
 //GET Route for notes page 
-app.get('/notes', (req, res) =>
-    res.sendFile(__dirname, 'public/assets/notes.html'));
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 
-const { title, text } = req.body;
+//write and add notes into db.json   
+
 
 //if all the required properties are present
-if (title && text) {
-    const newNotes = {
-        title,
-        text, 
-        notes_id: uuid(),
-    };
+// if (title && text) {
+//     const newNotes = {
+//         title,
+//         text, 
+//         notes_id: uuid(),
+//     };
 
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-        } else {
-            const parsedNotes = JSON.parse(data);
+//     fs.readFile('./db/db.json', 'utf8', (err, data) => {
+//         if (err) {
+//             console.error(err);
+//         } else {
+//             const parsedNotes = JSON.parse(data);
 
-            //Add new note
-            parsedNotes.push(newNotes);
+//             Add new note
+//             parsedNotes.push(newNotes);
 
-        //Write updated reviews back to the file
-    fs.writeFile(
-        './db/db.json',
-        JSON.stringify(parsedNotes, null, 3),
-        (writeErr) =>
-        writeErr
-        ? console.error(writeErr)
-        : console.info('Successfully updated notes!')
-         );
-}
-});
-};
-
-
+//         Write updated reviews back to the file
+//     fs.writeFile(
+//         './db/db.json',
+//         JSON.stringify(parsedNotes, null, 3),
+//         (writeErr) =>
+//         writeErr
+//         ? console.error(writeErr)
+//         : console.info('Successfully updated notes!')
+//          );
+// }
+// });
+// };
 
 
+//Delete Route to delete note throug query parameter containing ID
+//To delete note, need to read all notes from db.json file
+//remove the note with given ID property
+//rewrite the notes to the db.json file
+// app.delete('/api/notes/:id' (req, res) => {
+//     res.send('${uuid} has been deleted')
+// });
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
